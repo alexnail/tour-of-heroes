2cc4376db2155964452b78061ffa0696428b4b24
eirafle <aleksandrs.fridmans@ericsson.com>
2021-08-13 18:25:02 +0100

    delete hero action

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -28,4 +28,9 @@ export class HeroesComponent implements OnInit {
       hero => {this.heroes.push(hero);}
     )
   }
+
+  delete(hero: Hero) {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 }
