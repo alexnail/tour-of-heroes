f14a652ea9dd8035443be626c052053c5b0559f2
eirafle <aleksandrs.fridmans@ericsson.com>
2021-08-13 18:00:04 +0100

    add hero action

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
 import {HeroService} from "../hero.service";
-import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -20,5 +19,13 @@ export class HeroesComponent implements OnInit {
   private getHeroes() {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string) {
+    name = name.trim();
+    if (!name) return;
+    this.heroService.addHero({name} as Hero).subscribe(
+      hero => {this.heroes.push(hero);}
+    )
   }
 }
